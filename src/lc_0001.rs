8f3a3d73771852b64f4d60739b0517336f16987d
yinheli <me@yinheli.com>
2020-09-27 14:30:08 +0800

    update 0001.Two-Sum
    add assert

--- src/lc_0001.rs
+++ src/lc_0001.rs
@@ -14,5 +14,5 @@ fn two_sum(num: &[i32], target: i32) -> Option<[usize; 2]> {
 #[test]
 fn two_sum_test() {
     let result = two_sum(&[2, 7, 11, 15], 9);
-    println!("{:?}", result)
+    assert_eq!(Some([0, 1]), result);
 }
