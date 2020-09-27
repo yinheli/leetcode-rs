0c13c9638e602473a0d1ecd091f10e45f11f1950
yinheli <me@yinheli.com>
2020-09-27 14:26:44 +0800

    add 0001.Two-Sum

---
+++ src/lc_0001.rs
@@ -0,0 +1,18 @@
+#[allow(dead_code)]
+fn two_sum(num: &[i32], target: i32) -> Option<[usize; 2]> {
+    let n = num.len();
+    for i in 0..n {
+        for j in i..n {
+            if num[i] + num[j] == target {
+                return Some([i, j]);
+            }
+        }
+    }
+    return None;
+}
+
+#[test]
+fn two_sum_test() {
+    let result = two_sum(&[2, 7, 11, 15], 9);
+    println!("{:?}", result)
+}
