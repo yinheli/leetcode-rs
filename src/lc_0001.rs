2007aab2b25d9e0c47e948501af0715936ba84b9
yinheli <me@yinheli.com>
2020-09-28 13:31:26 +0800

    update tow_sum

--- src/lc_0001.rs
+++ src/lc_0001.rs
@@ -1,18 +1,31 @@
 #[allow(dead_code)]
-fn two_sum(num: &[i32], target: i32) -> Option<[usize; 2]> {
-    let n = num.len();
-    for i in 0..n {
-        for j in i..n {
-            if num[i] + num[j] == target {
-                return Some([i, j]);
+struct Solution {}
+
+// https://leetcode.com/problems/two-sum/
+
+impl Solution {
+    #[allow(dead_code)]
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let n = nums.len();
+        for i in 0..n {
+            for j in i + 1..n {
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
+                }
             }
         }
+        vec![]
     }
-    return None;
 }
 
 #[test]
-fn two_sum_test() {
-    let result = two_sum(&[2, 7, 11, 15], 9);
-    assert_eq!(Some([0, 1]), result);
+fn test_two_sum() {
+    let r = Solution::two_sum(vec![2, 7, 11, 15], 9);
+    assert_eq!(vec![0, 1], r);
+
+    let r = Solution::two_sum(vec![3, 2, 4], 6);
+    assert_eq!(vec![1, 2], r);
+
+    let r = Solution::two_sum(vec![3, 3], 6);
+    assert_eq!(vec![0, 1], r);
 }
